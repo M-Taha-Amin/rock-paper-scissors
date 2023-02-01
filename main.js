9259e24f9744b0cfcf9b4397cb7eaaa02857f513
M-Taha-Amin <tahaamin028@gmail.com>
2023-02-01 09:51:52 +0500

    Added a function to let user play a round
    Inside it,
    
    - Added a prompt to get user choice
    - A loop to check user choice validity

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ let computerScore = 0;
 function getComputerChoice() {
   return moves[Math.floor(Math.random() * 3)];
 }
+
+// Added a function to let user play a round
+function playRound(playerSelection, computerSelection) {
+  // Prompt for user choice
+  playerSelection = prompt("rock, paper or scissors?").toLowerCase();
+
+  // A loop to check user choice validity
+  while (!moves.includes(playerSelection)) {
+    playerSelection = prompt(
+      "Please choose a valid option: rock, paper or scissors?"
+    ).toLowerCase();
+  }
+}
