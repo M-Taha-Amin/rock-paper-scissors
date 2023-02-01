4645f9fb684026783d58f6eafbd23f5b4ee56fb3
M-Taha-Amin <tahaamin028@gmail.com>
2023-02-01 09:49:35 +0500

    Added a function for random computer choice

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const moves = ["rock", "paper", "scissors"];
 // Added score count to keep track of wins
 let playerScore = 0;
 let computerScore = 0;
+
+// Added a function for random computer choice
+function getComputerChoice() {
+  return moves[Math.floor(Math.random() * 3)];
+}
